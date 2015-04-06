a187574e2b56c61199455750cc6a11ca5878c5fd
Sue-Yi-FAN <x95102003@gmail.com>
2015-04-06 17:28:43 +0800

    Add error argument controler.

--- src/x95102003/HtmlMapReducer.java
+++ src/x95102003/HtmlMapReducer.java
@@ -18,39 +18,54 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import x95102003.HtmlParser;
 import x95102003.HtmlPageRank.mapper;
 import x95102003.HtmlPageRank.reducer;
+
 public class HtmlMapReducer {
-	public static class HtmlParseMapper extends Mapper<LongWritable, Text, Text, Text>{
-		public void map(LongWritable key, Text v, Context context) throws IOException, InterruptedException{
+	public static class HtmlParseMapper extends
+			Mapper<LongWritable, Text, Text, Text> {
+		public void map(LongWritable key, Text v, Context context)
+				throws IOException, InterruptedException {
 			StringTokenizer token = new StringTokenizer(v.toString());
-			String outcomeURL="";
-			String incomeURL ="";
-			while(token.hasMoreTokens()){
+			String outcomeURL = "";
+			String incomeURL = "";
+			while (token.hasMoreTokens()) {
 				String webCont = token.nextToken();
-				if(HtmlParser.getDomainName(webCont).length() > 0){
+				if (HtmlParser.getDomainName(webCont).length() > 0) {
 					outcomeURL = HtmlParser.getDomainName(webCont);
 				}
-				if(HtmlParser.parseHtml(webCont).length() > 0){
+				if (HtmlParser.parseHtml(webCont).length() > 0) {
 					incomeURL = HtmlParser.parseHtml(incomeURL);
 				}
-				if(incomeURL.length() > 0 && outcomeURL.length() > 0){
+				if (incomeURL.length() > 0 && outcomeURL.length() > 0) {
 					context.write(new Text(incomeURL), new Text(outcomeURL));
 				}
-				
+
 			}
 		}
 	}
-	public static class HtmlParseReducer extends Reducer<Text, Iterable<Text>, Text, Text>{
+
+	public static class HtmlParseReducer extends
+			Reducer<Text, Iterable<Text>, Text, Text> {
 		final int initPageRank = 1;
-		public void reduce(Text key,Iterable<Text> values, Context context) throws IOException, InterruptedException{
+
+		public void reduce(Text key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
 			String appedString = new String();
-			for (Text v : values){
-				appedString += v.toString()+ " ";
+			for (Text v : values) {
+				appedString += v.toString() + " ";
 			}
-			String mapperInput = String.valueOf(initPageRank) + " " + appedString;
+			String mapperInput = String.valueOf(initPageRank) + " "
+					+ appedString;
 			context.write(key, new Text(mapperInput));
 		}
 	}
-	public static void main(String args[]) throws IOException, ClassNotFoundException, InterruptedException{
+
+	public static void main(String args[]) throws IOException,
+			ClassNotFoundException, InterruptedException {
+		if (args.length != 2) {
+			System.out
+					.println("Error input format with arguments please type with two args");
+		}
+
 		Configuration conf = new Configuration();
 
 		Job job = new Job(conf, "Html Parse");
